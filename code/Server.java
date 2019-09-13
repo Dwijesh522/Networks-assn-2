dc32c05456dac4ba7879d33b1eefd3eefdda026a
praffulr <36444053+praffulr@users.noreply.github.com>
2019-09-13 21:47:11 +0530

    fixed symbol issue

--- code/Server.java
+++ code/Server.java
@@ -4,11 +4,15 @@ import java.util.*;
 import java.lang.Thread;
 public class Server
 {
-
+///mode
+//1 -> unencrypted
+//2 -> encrypted
+//3 -> encrypted with signatures
   public static void main(String argv[]) //throws Exception
   {
     try
     {
+      int mode = Integer.parseInt(argv[0]);
       Hashtable<String, HashTableData> table = new Hashtable<String, HashTableData>();
       ServerSocket welcomeSocket_recv = new ServerSocket(6789); // Send port on server
       while(true)
@@ -16,7 +20,7 @@ public class Server
         Socket connectionSocket_recv = welcomeSocket_recv.accept();
         BufferedReader inFromClient_recv = new BufferedReader(new InputStreamReader(connectionSocket_recv.getInputStream()));
         DataOutputStream outToClient_recv = new DataOutputStream(connectionSocket_recv.getOutputStream());
-        Serverthread recv_thread = new Serverthread(table, connectionSocket_recv, inFromClient_recv, outToClient_recv);
+        Serverthread recv_thread = new Serverthread(mode, table, connectionSocket_recv, inFromClient_recv, outToClient_recv);
         Thread r_thread = new Thread(recv_thread);
         r_thread.start();
       }
